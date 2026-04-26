3ab4e50f33a0f8141d0fc230e0f1d6abe53739d1
justagooseidek <ishaqwarrior234@gmail.com>
2026-04-26 12:39:25 +0530

    Added eleventy config to define input directory

---
+++ .eleventy.js
@@ -0,0 +1,9 @@
+module.exports = function (eleventyConfig) {
+  // This tells 11ty to look in your current folder for files
+  return {
+    dir: {
+      input: ".",
+      output: "_site",
+    },
+  };
+};
